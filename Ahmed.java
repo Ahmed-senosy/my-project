e9824d35def07ae06c0a4ae73fde96af88ceb077
Your Name <you@example.com>
2023-12-05 08:18:42 +0200

    master

---
+++ Ahmed.java
@@ -0,0 +1,10 @@
+public class Ahmed {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Ahmed Mohamed Senosy");
+        
+    }
+    
+}
---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /app
+
+COPY Ahmed.java .
+
+RUN javac Ahmed.java
+
+CMD java Ahmed
